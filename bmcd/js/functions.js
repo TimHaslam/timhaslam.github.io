097357b257481ad587d50155f8fd56810252f0c5
Tim Haslam <tim@codegreene.com>
2020-04-29 22:09:57 -0600

    updates to bmcd

--- bmcd/js/functions.js
+++ bmcd/js/functions.js
@@ -1,17 +1,23 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+$( document ).ready(function() {
+	
+	$(window).scroll(function() {    
+	    var scroll = $(window).scrollTop();
 
-// Get the navbar
-var navbar = document.getElementById("nav");
+	    if (scroll >= 40) {
+	        $("header").addClass("sticky");
+	    } else {
+	        $("header").removeClass("sticky");
+	    }
+	});
+	
+	$(window).scroll(function() {    
+	        var $myDiv = $('.body-bg');   
+	        var st = $(this).scrollTop();
+			var divHeight = 550 - st;
+	        $myDiv.css("height", divHeight );         
+	           
+	        })
+	    .scroll();
+});
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
