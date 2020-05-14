70d478447aec6acac288b75493cb16587a958f41
Tim Haslam <tim@codegreene.com>
2020-05-13 21:42:49 -0600

    updates

--- bmcd/js/functions.js
+++ bmcd/js/functions.js
@@ -3,46 +3,30 @@ $( document ).ready(function() {
 	$('.accordion-bar').click(function(){
 	      $('.accordion-panel', this).toggle('slow');
 	  });
-
+	
 	$('.accordion-bar').click(function(){
 	      $(this).toggleClass("open");
 	  });
 	
-	$(window).scroll(function() {    
-	    var scroll = $(window).scrollTop();
-
-	    if (scroll >= 40) {
-	        $("header").addClass("sticky");
-	    } else {
-	        $("header").removeClass("sticky");
-	    }
+	// 	$(window).scroll(function() {    
+	// 	    var scroll = $(window).scrollTop();
+	// 
+	// 	    if (scroll >= 40) {
+	// 	        $("header").addClass("sticky");
+	// 	    } else {
+	// 	        $("header").removeClass("sticky");
+	// 	    }
+	// 	});
+	
+	
+	$('.toggle-text').click(function(){
+		$(this).toggleClass("open");
 	});
 	
-	
-	
-	$(window).scroll(function() {    
-	        var $myDiv = $('.body-bg');   
-	        var st = $(this).scrollTop();
-			var divHeight = 550 - st;
-	        $myDiv.css("height", divHeight );         
-	           
-	        })
-	    .scroll();
-	
-		$(window).scroll(function() {    
-		        var $myDiv = $('.body-bg.small');   
-		        var st = $(this).scrollTop();
-				if ($(window).width() < 600) {
-				   var divHeight = 265 - st;
-				}
-				else {
-				   var divHeight = 365 - st;
-				}
-				
-		        $myDiv.css("height", divHeight );         
-
-		        })
-		    .scroll();
+	$('.nav').slicknav({
+		prependTo: 'header .container',
+		label: ''
+	});
 });
 
 
